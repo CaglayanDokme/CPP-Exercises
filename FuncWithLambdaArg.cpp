71e640d97e1adb115900cd644d0f0a82c6ca163e
CaglayanDokme <caglayandokme@gmail.com>
2021-02-22 16:04:55 +0300

    Examples enhanced.

--- FuncWithLambdaArg.cpp
+++ FuncWithLambdaArg.cpp
@@ -1,4 +1,4 @@
-// Description: Dummy code for functions that accept lambda expressions as input parameter
+// Description: Function examples with lambda expressions as input parameters
 // Date:        February 20, 2021
 // Author:      Caglayan DOKME
 
@@ -8,19 +8,43 @@
 
 using namespace std;
 
-template<typename LambdaType>
-void Function(vector<int>& container, LambdaType lambda)
+template<typename ContainerType, typename LambdaType>
+void Function(ContainerType& container, LambdaType lambda)
 {
-    /*  Iterate over the given integer vector and call the lambda expression using the pointed data. */
-    for(vector<int>::iterator itr = container.begin(); itr != container.end(); itr++)
-        if(lambda(*itr) == true)
-            cout << *itr << endl;
+    /*  Iterate over the given container and call the lambda expression with the current element. */
+    for(auto element : container)
+        if(lambda(element) == true)
+            cout << element << endl;
 }
 
 int main(int argc, char const *argv[]) {
     vector<int> v1{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+    /*  Example Lambda Expression:
+     *  [capture list] (parameter list) -> return type {function body} */
+
+    /** Inline Lambda Expression **/
+    cout << "Printing values(>5) with inline lambda: " << endl;
     Function(v1, [](int value) { return (value > 5); });
+    cout << endl;
+
+    /** Lambda Expression Object **/
+    auto lambdaFunc =   []                          /* Capture list is empty    */
+                        (int value)                 /* Function-like parameters */
+                        { return (value < 5); };    /* Function body            */
+    cout << "Printing values(<5) with lambda expression object: " << endl;
+    Function(v1, lambdaFunc);
+    cout << endl;
+
+    /** Lambda Expression Object with Capture List **/
+    const int limit = 3;
+    auto lambdaFuncCapture =    [limit]                         /* Capture list is empty    */
+                                (int value)                     /* Function-like parameters */
+                                { return (value > limit); };    /* Function body            */
+    cout << "Printing values(>limit(" << limit
+         << ")) using lambda expression with non-empty capture list : " << endl;
+    Function(v1, lambdaFuncCapture);
+    cout << endl;
 
     return 0;
 }
